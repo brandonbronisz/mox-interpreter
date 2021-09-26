41e6f327e5d344149a462d4361544810472adc3f
Brandon Bronisz <brandon.bronisz.96@gmail.com>
2021-09-26 09:44:05 -0400

    Update Mox expressions generator script

--- src/main/java/com/mox/tools/GenerateAst.java
+++ src/main/java/com/mox/tools/GenerateAst.java
@@ -26,17 +26,22 @@ public class GenerateAst {
         System.out.println(path);
         PrintWriter writer = new PrintWriter(path, "UTF-8");
 
-        writer.println("package com.java.interpreter;");
+        writer.println("package com.mox.interpreter;");
         writer.println();
         writer.println("import java.util.List;");
         writer.println();
         writer.println("abstract class " + baseName + " {");
+
+        defineVisitor(writer, baseName, types);
 
         for (String type: types) {
             String className = type.split(":")[0].trim();
             String fields = type.split(":")[1].trim();
             defineType(writer, baseName, className, fields);
         }
+
+        writer.println();
+        writer.println("    abstract <R> R accept(Visitor<R> visitor);");
 
         writer.println("}");
         writer.close();
@@ -57,6 +62,12 @@ public class GenerateAst {
 
         writer.println("    }");
 
+        writer.println();
+        writer.println("    @Override");
+        writer.println("    <R> R accept(Visitor<R> visitor) {");
+        writer.println("    return visitor.visit" + className + baseName + "(this);");
+        writer.println("     }");
+
         // Fields
         writer.println();
         for (String field: fields) {
@@ -64,5 +75,16 @@ public class GenerateAst {
         }
 
         writer.println("  }");
+    }
+
+    private static void defineVisitor(PrintWriter writer, String baseName, List<String> types) {
+        writer.println("    interface Visitor<R> {");
+
+        for (String type: types) {
+            String typeName = type.split(":")[0].trim();
+            writer.println("    R visit" + typeName + baseName + "(" + typeName + " " + baseName.toLowerCase() + ");");
+        }
+
+        writer.println("    }");
     }
 }
